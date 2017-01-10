5dd8eb9c8988e8e4250290fa06aa3d7f6941a5b1
GokhanArpakci <Gokhan.Arpakci@mail-bip.com>
2017-01-10 15:19:25 +0100

    Update assesment.R

--- assesment.R
+++ assesment.R
@@ -39,3 +39,22 @@ makeCacheMatrix <- function(x = numeric()) {
         # return a list. Each named element of the list is a function
         list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
+# The next function obtains the inverse of a "special" matrix created with 
+# makeCacheMatrix
+cacheSolve <- function(y, ...) {
+        # get the cached value
+        inverse <- y$getInverse()
+        # if a cached value exists return it
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        # otherwise get the matrix, caclulate the inverse and store it in
+        # the cache
+        data <- y$getMatrix()
+        inverse <- solve(data)
+        y$cacheInverse(inverse)
+        
+        # return the inverse
+        inverse
+}
